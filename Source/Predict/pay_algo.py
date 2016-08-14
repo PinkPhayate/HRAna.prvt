5e7a8fc93463968aadbeb2a99c0759eeacfdb1d4
Tanaka Hayate <pinqphayate@gmail.com>
2016-08-14 15:41:39 +0900

    add module to write report

--- Source/Predict/pay_algo.py
+++ Source/Predict/pay_algo.py
@@ -1,20 +1,28 @@
 # coding: UTF-8
 import pandas as pd
 import csv,re,json,itertools
-THRESHOLD = 0.5
+THRESHOLD = 0.7
 RED = '\033[93m'
 GREEN = '\033[92m'
 ENDC = '\033[0m'
 
 def collate_pred():
+    report = open('./../../Result/Report/model1_report.txt',"a+")
+    report.write('THRESHOLD:\t' + str(THRESHOLD))
+    report.write('\nMODEL:\tSGD')
+
+    total_year = 0.0
+    balance = 0
+
     f = open('./../../Result/sgd_default_prob.csv', 'r')
     dataReader = csv.reader(f)
     for row in dataReader:
-        s = pd.Series(row)
-        num, odds = get_valid_comb(s[0])
-
-        balance = 0
         index = 0
+        benefit = 0
+
+
+        s = pd.Series(row)
+        num, odds = get_valid_comb(s[0])    # s[0] -> race_id
 
         res = s[1:].astype(float)
         res = res[res > THRESHOLD]
@@ -28,11 +36,21 @@ def collate_pred():
                 index += 1
                 comb = str(element[0]) + '-' + str(element[1])
                 if comb in num:
-                    balance += odds[num.index(comb)]
-                    print GREEN + 'RETURN' + str( odds[num.index(comb)] ) + ENDC
+                    benefit += odds[num.index(comb)]
 
-        balance  -= index * 100
-        print RED + 'TOTAL: ' + str(balance) + ENDC
+            balance  -= index * 100
+        print GREEN + 'race_id: ' + str(s[0]) + '\tCOMBINATION: ' + str(index) + '\tRETURN: ' + str( benefit ) + ENDC
+        report.write('\nrace_id: ' + str(s[0]) + '\tCOMBINATION: ' + str(index) + '\tRETURN: ' + str( benefit ))
+        balance += benefit
+        total_year += 1.0
+
+    report.write('\n======================================================================================')
+    print RED + 'BALANCE:\t' + str(balance) + ENDC
+    report.write('\nBALANCE:\t' + str(balance) )
+    print RED + 'AVERAGE:\t' + str( balance/total_year ) + ENDC
+    report.write('\nAVERAGE:\t' + str( balance/total_year ) )
+    report.write('\n\n')
+    report.close()
 
 
 def get_valid_comb(race_id):
@@ -53,7 +71,6 @@ def get_valid_comb(race_id):
         odds = map(int,odds)
         # print odds
     return num, odds
-
 
 if __name__ == '__main__':
     collate_pred()
