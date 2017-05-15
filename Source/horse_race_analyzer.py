c27b7db772d66790d0f108ffe03e5cb80f872c66
Tanaka Hayate <pinqphayate@gmail.com>
2017-05-15 21:42:14 +0900

    各年のデータフレームを結合するモジュールの記述

---
+++ Source/horse_race_analyzer.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from Model import race
+import mysql_connector
+import pandas as pd
+
+# mysqlのインスタンスを作成
+mysql_conn = mysql_connector.MYSQL_connector()
+
+# 対象レースの過去のレースのidを取得
+rids = [201602010211,201606040811]
+
+# モデル作成のためのデータフレーム作成
+df = pd.DataFrame([])
+for rid in rids:
+    r = race.Race(rid, mysql_conn)
+    df = pd.concat([df, r.df], axis=1)
+print df
